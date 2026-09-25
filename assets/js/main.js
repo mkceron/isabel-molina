e5c18514c47770d5404cd9e1abecaaf9e7ca6d52
mkceron <mkceron@gmail.com>
2026-09-24 20:19:40 -0400

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,1 @@
-
+document.getElementById('year').textContent=new Date().getFullYear();const b=document.querySelector('.menu'),n=document.querySelector('.site-header nav');b.addEventListener('click',()=>n.classList.toggle('open'));n.querySelectorAll('a').forEach(a=>a.addEventListener('click',()=>n.classList.remove('open')));
